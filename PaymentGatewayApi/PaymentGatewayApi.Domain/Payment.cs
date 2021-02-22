358badcf65a57fc872cc50ee12fc464eaba8cf0a
Vasco Soares <vascosoares.online@gmail.com>
2021-02-22 01:12:08 +0000

    Payment Gateway first draft

---
+++ PaymentGatewayApi/PaymentGatewayApi.Domain/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PaymentGatewayApi.Domain
+{
+    public partial class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid AuthorizationId { get; set; }
+        public int PaymentState { get; set; }
+        public Guid PaymentGuid { get; set; }
+        public string CardNumber { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public string CVV { get; set; }
+    }
+}
---
+++ PaymentGatewayApi/PaymentGatewayApi.Messaging.Send/Sender/v1/PaymentCreateSender.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using PaymentGatewayApi.Domain;
+using PaymentGatewayApi.Messaging.Send.Options.v1;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace PaymentGatewayApi.Messaging.Send.Sender.v1
+{
+    public class PaymentCreateSender : IPaymentCreateSender
+    {
+        private readonly string _hostname;
+        private readonly string _password;
+        private readonly string _queueName;
+        private readonly string _username;
+        private IConnection _connection;
+
+        public PaymentCreateSender(IOptions<RabbitMqConfiguration> rabbitMqOptions)
+        {
+            _queueName = rabbitMqOptions.Value.QueueName;
+            _hostname = rabbitMqOptions.Value.Hostname;
+            _username = rabbitMqOptions.Value.UserName;
+            _password = rabbitMqOptions.Value.Password;
+
+            CreateConnection();
+        }
+
+        public void SendPaymentCreate(Payment payment)
+        {
+            if (ConnectionExists())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var json = JsonConvert.SerializeObject(payment);
+                    var body = Encoding.UTF8.GetBytes(json);
+
+                    channel.BasicPublish(exchange: "", routingKey: _queueName, basicProperties: null, body: body);
+                }
+            }
+        }
+
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = _hostname,
+                    UserName = _username,
+                    Password = _password
+                };
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not create connection: {ex.Message}");
+            }
+        }
+
+        private bool ConnectionExists()
+        {
+            if (_connection != null)
+            {
+                return true;
+            }
+
+            CreateConnection();
+
+            return _connection != null;
+        }
+    }
+}
