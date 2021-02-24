ec27b94392487998827fd9948031fdc6a360d0a9
Vasco Soares <vascosoares.online@gmail.com>
2021-02-24 01:01:37 +0000

    Ãdded Acquiring Bank service mock

---
+++ AcquiringBankMock/AcquiringBankMock/Payment.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AcquiringBankMock
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public int PaymentState { get; set; }
+        public Guid PaymentGuid { get; set; }
+        public string CardNumber { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public string CVV { get; set; }
+    }
+}
